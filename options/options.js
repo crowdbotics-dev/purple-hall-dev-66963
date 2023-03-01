962b9e5b0bd50fb3de1a2d98ae1b9a7c495d8e8b
Crowdbotics <engineering@crowdbotics.com>
2023-03-01 00:16:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "purple-hall-dev-66963",
+  url: "https://purple-hall-dev-66963.botics.co",
+  api: "https://purple-hall-dev-66963.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
